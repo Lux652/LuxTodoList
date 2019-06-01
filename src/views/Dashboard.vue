c6a4016904f6701514b20c5e53616e504ff53ffa
Luka <lukakovacicster@gmail.com>
2019-06-01 20:36:17 +0200

    +Projects.vue, +Team.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,24 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+
+        <v-tooltip top>
+          <v-btn slot = "activator" small flat color="grey" @click="sortBy('title')">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">By project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <v-btn slot="activator" small flat color="grey" @click="sortBy('person')">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">By person</span>
+          </v-btn>
+          <span>Sort projects by person</span>
+        </v-tooltip>
+      </v-layout>
 
       <v-card v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -20,7 +38,10 @@
           </v-flex>
           <v-flex xs2 sm4 md2>
             <div class="right">
-              <v-chip small :class="`${project.status} white--text camption my-2`">{{project.status}}</v-chip>
+              <v-chip
+                small
+                :class="`${project.status} white--text camption my-2`"
+              >{{project.status}}</v-chip>
             </div>
           </v-flex>
         </v-layout>
@@ -32,36 +53,69 @@
 
 <script>
 export default {
-  data(){
-    return{
-      projects:[
-        {title: 'New website', person: 'Majstor Lux', due:'05.06.2019.', status:'ongoing', content:'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!'},
-        {title: 'New Footer', person: 'Majstor Mrkonja', due:'02.06.2019.', status:'complete', content:'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!'},
-        {title: 'New header', person: 'Majstor Brajstor', due:'03.06.2019.', status:'complete', content:'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!'},
-        {title: 'New whatever', person: 'Majstor Koji', due:'01.06.2019.', status:'overdue', content:'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!'},
+  data() {
+    return {
+      projects: [
+        {
+          title: "ANew website",
+          person: "Majstor Luka",
+          due: "05.06.2019.",
+          status: "ongoing",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New Footer",
+          person: "Majstor Mrkonja",
+          due: "02.06.2019.",
+          status: "complete",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New header",
+          person: "Majstor Brajstor",
+          due: "03.06.2019.",
+          status: "complete",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New whatever",
+          person: "Majstor Koji",
+          due: "01.06.2019.",
+          status: "overdue",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        }
       ]
+    };
+  },
+  methods: {
+    sortBy(p) {
+      this.projects.sort((a, b) => (a[p] < b[p] ? -1 : 1));
     }
   }
 };
 </script>
 
 <style>
-.project.complete{
-  border-left:4px solid #3cd1c2
+.project.complete {
+  border-left: 4px solid #3cd1c2;
 }
-.project.ongoing{
-  border-left:4px solid orange
+.project.ongoing {
+  border-left: 4px solid orange;
 }
-.project.overdue{
-  border-left:4px solid tomato
+.project.overdue {
+  border-left: 4px solid tomato;
 }
-.v-chip.complete{
+.v-chip.complete {
   background: #3cd1c2;
 }
-.v-chip.ongoing{
+.v-chip.ongoing {
   background: #ffaa2c;
 }
-.v-chip.overdue{
+.v-chip.overdue {
   background: #f83e70;
 }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,10 +1,70 @@
 <template>
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
-    <v-container class="my-5">content</v-container>
+
+    <v-container class="my-5">
+
+      <v-expansion-panel>
+        <v-expansion-panel-content v-for="project in myProjects" :key="project.title">
+          <div slot="header">{{project.title}}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{project.due}}</div>
+              <div>{{project.content}}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{
+      projects: [
+        {
+          title: "New website",
+          person: "Majstor Luka",
+          due: "05.06.2019.",
+          status: "ongoing",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New Footer",
+          person: "Majstor Luka",
+          due: "02.06.2019.",
+          status: "complete",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New header",
+          person: "Majstor Brajstor",
+          due: "03.06.2019.",
+          status: "complete",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        },
+        {
+          title: "New whatever",
+          person: "Majstor Koji",
+          due: "01.06.2019.",
+          status: "overdue",
+          content:
+            "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dicta aliquid explicabo!"
+        }
+      ]
+    }
+  },
+  computed:{
+    myProjects(){
+      return this.projects.filter(project =>{
+        return project.person === 'Majstor Luka'
+      })
+    }
+  }
+};
 </script>
